cac3b48c7aa8f9458b7979154f8b939310036739
JakeNizio <jake.nizio2@gmail.com>
2024-11-19 21:13:59 -0500

    updated ex3.sql to include required Owner row

--- SQL/ex3.sql
+++ SQL/ex3.sql
@@ -1,9 +1,16 @@
+-- Owner needed first as Farm has a foreign key to Owner
+INSERT INTO Owner
+VALUES ('annesmith', 'password1', 'Anne Smith', 'annesmith@example.com', '2512512511', 0);
+
+-- Farm Insert 1: No columns specified
 INSERT INTO Farm
 VALUES (0, 1234560002, 'Ontario', 'London', 'Richmond St', 1511, 1);
 
+-- Farm Insert 2: Columns specified, and some values left null
 INSERT INTO Farm (phoneNumber, province, city, ownerID)
 VALUES (1234560003, 'Ontario', 'Windsor', 1);
 
+-- Farm Insert 3: Columns specified and phoneNumber selected from Owner table
 INSERT INTO Farm (phoneNumber, province, city, ownerID)
 VALUES (
 	(SELECT phoneNumber FROM owner WHERE ownerID = 1),
@@ -11,6 +18,7 @@ VALUES (
     'Chatham',
     1
     );
-        
+    
+-- Show the results of all three inserts
 SELECT *
 FROM Farm;
