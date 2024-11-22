b40e87b8fa51a3f8942086162789d03c252ae887
JakeNizio <jake.nizio2@gmail.com>
2024-11-22 11:41:53 -0500

    adjusted ex6.sql modification 2

--- SQL/ex6.sql
+++ SQL/ex6.sql
@@ -1,4 +1,4 @@
--- Panels that have been installed for more than 30 days and are active will be give nan energy produced value of 0.00 with todays date
+-- Panels that have been installed for more than 30 days and are active will be given an energy produced value of 0.00 with todays date
 INSERT INTO EnergyProduced (currentDate, panelID, energyProduced)
 SELECT CURDATE(), panelID, 0.00
 FROM Panel
@@ -7,10 +7,14 @@ WHERE panelStatus = 'active'
   AND panelID NOT IN (SELECT panelID FROM EnergyProduced WHERE currentDate = CURDATE());
 
 -- Updates all panels that are inactive and have been installed for more than 35 days to active
-UPDATE Panel
-SET panelStatus = 'active'
-WHERE panelStatus = 'inactive'
-  AND DATEDIFF(CURDATE(), installationDate) > 35;
+UPDATE Panel p1
+INNER JOIN (
+    SELECT panelID
+    FROM Panel
+    WHERE panelStatus = 'inactive'
+      AND DATEDIFF(CURDATE(), installationDate) > 35
+) p2 ON p1.panelID = p2.panelID
+SET p1.panelStatus = 'active';
 
 -- Deletes the tuples for panels in the energy produced table whose status is active and have been installed for more than 35 days
 DELETE FROM energyproduced
